4b7abc2344308ccb29ebcc586923ca35cced5716
EvlDandy <EvlDandy@gmail.com>
2016-03-16 21:56:33 +0900

    Another test
    Added some tiles and such, nothing crazy, just basic tutorials online.

--- main.lua
+++ main.lua
@@ -11,9 +11,30 @@ debug = true
 -- Game Data
 
 function love.load()
+  -- initial setup
+  visitorFont = love.graphics.newFont('assets/visitor1.ttf', 14)
+  love.graphics.setFont(visitorFont)
   width = love.graphics.getWidth()
   height = love.graphics.getHeight()
+  love.graphics.setBackgroundColor(0, 0, 255)
+  vin = love.graphics.newImage('assets/images/vin.png')
 
+  -- tile redyieeer
+  Tileset = love.graphics.newImage('assets/images/countryside.png')
+  TileW, TileH = 32, 32
+  local tilesetW, tilesetH = Tileset:getWidth(), Tileset:getHeight()
+  Quads = {
+    love.graphics.newQuad(0, 0, TileW, TileH, tilesetW, tilesetH), -- 1 = grass
+    love.graphics.newQuad(32, 0, TileW, TileH, tilesetW, tilesetH), -- 2 = box
+    love.graphics.newQuad(0, 32, TileW, TileH, tilesetW, tilesetH), -- 3 = flowers
+    love.graphics.newQuad(32, 32, TileW, TileH, tilesetW, tilesetH) -- 4 = boxtop
+  }
+  MyGlobalTable = {}
+  TileTable = {
+    { 1,1,2 },
+    { 1,2,1 },
+    { 1,1,1 }
+  }
 end
 
 function love.update(dt)
@@ -23,14 +44,23 @@ function love.update(dt)
   end
 
   if love.keyboard.isDown("up") then
-    camera.y = camera.y + 1
+    camera.y = camera.y - 1
   end
 end
 
 function love.draw()
   camera:set()
   -- here be things
+  love.graphics.setColor(255, 255, 255)
   love.graphics.print('Go Fuck yourself', 0, 0)
-  camera:unset()
+  love.graphics.draw(vin, camera.x, camera.y)
 
+  for rowIndex=1, #TileTable do
+    local row = TileTable[rowIndex]
+    for columnIndex=1, #row do
+      local number = row[columnIndex]
+      love.graphics.draw(Tileset, Quads[number], (columnIndex-1)*TileW, (rowIndex-1)*TileH)
+    end
+  end
+  camera:unset()
 end
